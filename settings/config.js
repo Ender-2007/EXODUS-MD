5eca747fccccf113ea8c71b02865ad3266fb55fc
Emma2010lgtm <bissielomakokidianechrystelle@gmail.com>
2025-07-17 23:33:53 +0000

    Create config.js

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['241077739360']
+global.gambar = "https://iili.io/Fc5DZaj.md.jpg"
+global.packname = "EXODUS-MD"
+global.author = "Joker"
+global.auto_recording = true
